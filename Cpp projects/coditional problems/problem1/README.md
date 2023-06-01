1ee0c05802ea09e75c7e3bf90aadd1c1398a3452
Mostafa Akram <mostafaakram74@gmail.com>
2023-06-02 02:21:17 +0300

    Cpp projects and tasks folder added

---
+++ Cpp projects/coditional problems/problem1/README.md
@@ -0,0 +1,31 @@
+# Coditional problem
+
+### this Cpp program Given a positive integer , do the following
+
+##### If 9<= n >=1 , print the lowercase English word corresponding to the number (e.g., one for 1, two for 2, etc.)
+
+##### If n>9 , print Greater than 9
+
+# Sample Input 0
+
+    5
+
+# Sample Output 0
+
+    five
+
+# Sample Input 1
+
+    8
+
+# Sample Output 1
+
+    eight
+
+# Sample Input 2
+
+    44
+
+# Sample Output 2
+
+    Greater than 9
---
+++ Cpp projects/coditional problems/problem1/printingNumsLetters.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+
+    cin>>x;
+    if(x == 1)
+    {
+        cout<<"one \n";
+    }else if(x == 2)
+    {
+        cout<<"two \n";
+    }else if(x == 3)
+    {
+        cout<<"three \n";
+    }else if(x == 4)
+    {
+        cout<<"four \n";
+    }else if(x == 5)
+    {
+        cout<<"five \n";
+    }else if(x == 6)
+    {
+        cout<<"six \n";
+    }else if(x == 7)
+    {
+        cout<<"seven \n";
+    } else if(x == 8)
+    {
+        cout<<"eight \n";
+    } else if(x == 9)
+    {
+        cout<<"nine \n";
+    }  else{
+        cout<<"Greater than 9 \n";
+
+    }
+   
+    return 0;
+}
